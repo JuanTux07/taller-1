ce2f1961ca92519e9d480c81ce236b77ffd9e258
Juan David <cybera0840@gmail.com>
2016-04-04 22:39:33 -0500

    Proyecto final

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,13 @@
 #include "Arbitro.h"
 using namespace std;
 
+#include "Arbitro.h"
+#include "Tablero.h"
+using namespace std;
+
 int main() {
-    Arbitro arbitro = Arbitro();
-    arbitro.iniciarJuego();
-	return 0;
+  Arbitro*  arb=new Arbitro();
+  arb->iniciarJuego();
+
+    return 0;
 }
